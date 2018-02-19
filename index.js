ce1beb74bb3974a274192eed318947c3e187441a
hnguy0221 <hnguy0221@gmail.com>
2018-02-19 11:50:16 -0700

    enabled cors to fix a request error

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express(); //create an instance of express
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //DB setup
 mongoose.connect('mongodb://localhost/auth');
 
 //App setup
 app.use(morgan('combined')); //morgan and body-parser are known as middlewares in express. Morgan is logging framework. Body-parser is used to parse incoming request as json
+app.use(cors()); //to enable all cors for entire the application. Cors is a midleware to handle cors. To fix error, XMLHttpRequest cannot load http://localhost:3090/signin.
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
